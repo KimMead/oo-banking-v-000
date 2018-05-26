fcb9e19d664ab71e93045d6f534913bfd9f1aa2e
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-05-26 19:14:19 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,14 +14,14 @@ end
 
 def execute_transaction
   if @sender.balance > @amount && @status == "pending"
-    @sender.balance -= @amount 
-    @receiver.balance += @amount 
+    @sender.balance -= @amount
+    @receiver.balance += @amount
     @status = "complete"
-  else 
+  else
     @status = "rejected"
     return "Transaction rejected.  Please check your account balance."
   end
-end 
+
 end
 
 
