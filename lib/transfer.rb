9f7eb2c5a1fe69ad405ea2fa8e7d3e8eae58a110
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-05-26 19:06:39 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -5,6 +5,6 @@ def initialize(sender, receiver, transfer_amount)
   @sender = sender
   @receiver = receiver
   @status = "pending"
-  @transfer_amount = transfer_amount 
+  @transfer_amount = transfer_amount
 end
 end
