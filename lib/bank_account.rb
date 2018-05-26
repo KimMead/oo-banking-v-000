b6020a178f41e3904550bf4622b5050d70c5f717
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-05-26 16:38:16 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,9 @@
 class BankAccount
-  attr_accessor :name
+  
 
-  def initialize(bank_account, name)
+  def initialize(bank_account)
     @bank_account = bank_account
-    @name = name
+  
 
 end
 end
